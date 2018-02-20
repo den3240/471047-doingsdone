0816c71b52f61fc1818bdb054a68e5420040d1a2
Vlad <rybak.vlad00@gmail.com>
2018-02-20 16:34:41 +0200

    Исправил создание таблиц

--- schema.sql
+++ schema.sql
@@ -1,48 +1,33 @@
-CREATE DATABASE doingsdone;
+CREATE DATABASE `doingsdone`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-USE doingsdone;
+USE `doingsdone`;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(128),
-    email CHAR(128),
-    password CHAR(64),
-    contacts CHAR(128),
-    registration_date DATE
+CREATE TABLE `users` (
+	`id`  NOT NULL AUTO_INCREMENT,
+	`name` char(128),
+	`email` (128) UNIQUE,
+	`password` char(64),
+	`contacts` char(128),
+	`registration_date` DATE,
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE projects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(128),
-    u_id CHAR(128)
+CREATE TABLE `projects` (
+	`id`  NOT NULL AUTO_INCREMENT,
+	`name` (128) UNIQUE,
+	`u_id` (128),
+	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(128),
-    file CHAR(128),
-    create_date DATE,
-    complete_date DATE,
-    deadline DATE,
-    u_id CHAR(128),
-    p_id CHAR(128)
+CREATE TABLE `tasks` (
+	`id`  NOT NULL AUTO_INCREMENT,
+	`name` (128) UNIQUE,
+	`file` char(128),
+	`create_date` DATE,
+	`complete_date` DATE,
+	`deadline` DATE,
+	`u_id` (128),
+	`p_id` (128),
+	PRIMARY KEY (`id`)
 );
-
-SELECT u.id, p.name AS 
-p_name 
-FROM users u INNER JOIN 
-projects p ON 
-p.u_id = u.id;
-
-SELECT u.id, t.name AS 
-t_name 
-FROM users u INNER JOIN 
-tasks t ON 
-t.u_id = u.id;
-
-SELECT p.id, t.name AS 
-t_name 
-FROM projects p INNER JOIN 
-tasks t ON 
-t.p_id = p.id;
