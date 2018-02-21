97e420a4c95ee6db9b2a02f2b0be81a1030c0f21
Vlad <rybak.vlad00@gmail.com>
2018-02-21 15:36:33 +0200

    Удалил коментарии

--- schema.sql
+++ schema.sql
@@ -37,39 +37,3 @@ ALTER TABLE `projects` ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REF
 ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
 
 ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk1` FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`);
-
-
--- CREATE TABLE `users` (
--- 	`id` int NOT NULL AUTO_INCREMENT,
--- 	`name` char(128) NOT NULL,
--- 	`email` char(128) NOT NULL UNIQUE,
--- 	`password` char(64) NOT NULL,
--- 	`registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
--- 	`contacts` char(128) NOT NULL,
--- 	PRIMARY KEY (`id`)
--- );
---
--- CREATE TABLE `projects` (
--- 	`id` int NOT NULL AUTO_INCREMENT,
--- 	`name` char(128) NOT NULL UNIQUE,
--- 	`user_id` int NOT NULL,
--- 	PRIMARY KEY (`id`)
--- );
---
--- CREATE TABLE `tasks` (
--- 	`id` int NOT NULL AUTO_INCREMENT,
--- 	`name` char(128) NOT NULL UNIQUE,
--- 	`file` char(128) NOT NULL,
--- 	`create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
--- 	`complete_date` DATETIME NOT NULL,
--- 	`deadline` DATETIME NOT NULL,
--- 	`user_id` int NOT NULL,
--- 	`project_id` int NOT NULL,
--- 	PRIMARY KEY (`id`)
--- );
---
--- ALTER TABLE `projects` ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
---
--- ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
---
--- ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk1` FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`);
