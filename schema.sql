4e2514290b2f3831c1c52385aba4507b0bb3664d
Vlad <rybak.vlad00@gmail.com>
2018-02-21 13:42:22 +0200

    Исправил ошибки в создании БД и таблиц

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE `users` (
 	`email` char(128) NOT NULL UNIQUE,
 	`password` char(64) NOT NULL,
 	`registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`contacts` char(128) NOT NULL,
+	`contacts` char(128),
 	PRIMARY KEY (`id`)
 );
 
@@ -23,10 +23,10 @@ CREATE TABLE `projects` (
 CREATE TABLE `tasks` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`name` char(128) NOT NULL UNIQUE,
-	`file` char(128) NOT NULL,
+	`file` char(128),
 	`create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`complete_date` TIMESTAMP NOT NULL,
-	`deadline` TIMESTAMP NOT NULL,
+	`complete_date` DATETIME,
+	`deadline` DATETIME,
 	`user_id` int NOT NULL,
 	`project_id` int NOT NULL,
 	PRIMARY KEY (`id`)
@@ -37,3 +37,39 @@ ALTER TABLE `projects` ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REF
 ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
 
 ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk1` FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`);
+
+
+-- CREATE TABLE `users` (
+-- 	`id` int NOT NULL AUTO_INCREMENT,
+-- 	`name` char(128) NOT NULL,
+-- 	`email` char(128) NOT NULL UNIQUE,
+-- 	`password` char(64) NOT NULL,
+-- 	`registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+-- 	`contacts` char(128) NOT NULL,
+-- 	PRIMARY KEY (`id`)
+-- );
+--
+-- CREATE TABLE `projects` (
+-- 	`id` int NOT NULL AUTO_INCREMENT,
+-- 	`name` char(128) NOT NULL UNIQUE,
+-- 	`user_id` int NOT NULL,
+-- 	PRIMARY KEY (`id`)
+-- );
+--
+-- CREATE TABLE `tasks` (
+-- 	`id` int NOT NULL AUTO_INCREMENT,
+-- 	`name` char(128) NOT NULL UNIQUE,
+-- 	`file` char(128) NOT NULL,
+-- 	`create_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+-- 	`complete_date` DATETIME NOT NULL,
+-- 	`deadline` DATETIME NOT NULL,
+-- 	`user_id` int NOT NULL,
+-- 	`project_id` int NOT NULL,
+-- 	PRIMARY KEY (`id`)
+-- );
+--
+-- ALTER TABLE `projects` ADD CONSTRAINT `projects_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+--
+-- ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+--
+-- ALTER TABLE `tasks` ADD CONSTRAINT `tasks_fk1` FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`);
