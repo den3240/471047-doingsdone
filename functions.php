38b461eaebfa25d1a33de777d71d5c1727c3c402
Vlad <rybak.vlad00@gmail.com>
2018-03-05 19:52:52 +0200

    Сделал документацию

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Функция возвращает html код шаблона с буфериацией
+ * @param $path
+ * Путь к шаблону
+ * @param $data
+ * Массив параметров
+ * @return string
+ */
 function include_template($path, $data) {
   if (!file_exists($path)) {
     return '';
@@ -12,7 +20,14 @@ function include_template($path, $data) {
 }
 
 
-
+/**
+ * Функция считает кол-во заданий в проекте
+ * @param array $task_list
+ * Массив заданий
+ * @param $project_id
+ * Идентификатор проекта
+ * @return int
+ */
 function task_counting ($task_list = [], $project_id) {
   $project_count = 0;
   foreach ($task_list as $key => $value) {
@@ -23,6 +38,12 @@ function task_counting ($task_list = [], $project_id) {
   return $project_count;
 }
 
+/**
+ * Функция проверки даты
+ * @param $date
+ * Дата
+ * @return bool
+ */
 function date_check($date) {
   date_default_timezone_set("Europe/Kiev");
   $end_date = strtotime($date);
@@ -34,6 +55,14 @@ function date_check($date) {
   }
 }
 
+/**
+ * Поиск пользователя по его email
+ * @param $user_email
+ * Email которые передает пользователь
+ * @param $users
+ * Массив с пользователями
+ * @return array
+ */
 function searchUserByEmail($user_email, $users) {
   foreach ($users as $key => $value) {
     if ($user_email == $value['email']) {
